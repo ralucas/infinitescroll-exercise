83b50e14d3c76a1237530260a91cd5361128720d
R.A. Lucas <richard@richardalucas.com>
2013-10-04 23:07:10 -0600

    second commit

--- js/main.js
+++ js/main.js
@@ -1,70 +1,88 @@
 $(document).ready(function(){
 
-	var dayOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-	var fulldate = {};
 
+	//variables
 	var today = new Date();
-
-	var day = today.getDay();
-	var month = today.getMonth();
-	var date = today.getDate();
-	var year = today.getFullYear();
-
-	var daysArr = [
-		{
-			dayOfWeek: day,
-			month: month,
-			date: date,
-			year: year,
-	}];
-
-	var today = new Date();
-	var tomorrow = new Date();
-	tomorrow.setDate(today.getDate()+1);
-
+	var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+	var months = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
 	var dateArr = [];
+	var divEachDay = $('#appendDay');
 
+	//populate funnction
 	var populateWeek = function(date, numDays){
-		var newDate;
-		for(var i = 0; i < numDays; i++){
-			newDate = date.setDate((date.getDate()+i));
-			dateArr.push(new Date(newDate));
-			$('#calendar').append(divEachDay);
+		for(var i = 86400000; i < numDays*86400000; i+=86400000){
+			var tomorrow = new Date();
+			var dg = date.getTime();
+			
+			tomorrow.setTime(dg+i);
+
+			var day = days[tomorrow.getDay()];
+			var month = months[tomorrow.getMonth()];
+			var numDate = tomorrow.getDate();
+			var year = tomorrow.getFullYear();
+
+			divEachDay.find('.dateDisplay').text(day+'\n '
+				+month+' '+numDate+', '+year);
+			$('#calendar').append(divEachDay.html());
+			dateArr.push(tomorrow);
 		}
-	}
+		return dateArr;
+	};
+
+	//emptyArray
+	var emptyArray = function(arr){
+		arr.length = 0;
+		return arr;
+	};
+
+	//modal event
+	// $('#modalSave').on('click', function(){
+	// 	$('#agendaModal').modal('hide');
+	// 	var inputAgenda = $('#agendaInfo').val();
+	// 	var time = $('#time').val();
+	// 	console.log('input',inputAgenda, 'time', time);
+	// 	$(this).closest('.day').find('.list-group').append('<a href='+
+	// 		'"#agendaModal" class="list-group-item agendaItem">'+
+	// 		'<span class="pull-left">'+time+'</span><span class="span-align">'+
+	// 		inputAgenda+'</span><span class="glyphicon glyphicon-edit pull-right">'+
+	// 		'</span></a>');
+	// 	$('#agendaInfo').val('');
+	// 	$('#time').val('');
+	// 	console.log($(this).closest('.day').find('.list-group').html());
+	// });
+
+	//add agenda item
+	$('#calendar').on('click', '.add-btn', function(){
+		console.log('test');
+		var inputAgenda = $('#agendaInfo').val();
+		console.log('input',inputAgenda);
+		$(this).parent().parent().parent().find('.list-group').append(+
+			'<li class="list-group-item agendaItem">'
+			+inputAgenda+'<span class="glyphicon glyphicon-edit pull-right">'+
+			'</span></li>');
+		$('#agendaInfo').val('');
+	});
+
+	//initial load function
+	populateWeek(today, 7);
+	
+	//hide the first div
+	$('#eachDay:first-of-type').hide();
 
 	$('#dateDisplay').text(today);
 
-	$('#modalSave').on('click', function(){
-		$('#agendaModal').modal('hide');
-		var inputAgenda = $('#agendaInfo').val();
-		var time = $('#time').val();
-		console.log('input',inputAgenda, 'time', time);
-		$('.list-group').append('<a href="#agendaModal" class="list-group-item agendaItem">'+
-			'<span class="pull-left">'+time+'</span><span class="span-align">'+inputAgenda+
-			'</span><span class="glyphicon glyphicon-edit pull-right"></span></a>');
+	//scroll append
+	$(window).on('scroll', function(){
+		var winHeight = $(window).height();
+		var docHeight = $(document).height();
+		var scrollTop = $(window).scrollTop();
 
-		$('#agendaInfo').val('');
-		$('#time').val('');
+		if((scrollTop + winHeight) === docHeight){
+			var lastDate = dateArr.pop();
+			console.log(lastDate);
+			emptyArray(dateArr);
+			populateWeek(lastDate, 7);
+		}
 	});
-
-	var divEachDay = $('#appendDay').html();
-
-	//$('#calendar').append(divEachDay);
-
-
-	populateWeek(today, 7);
-	
-
-	$(window).scroll(function(){
-		console.log($(window).scrollTop());
-
-		//how far it gets
-		// if((window).scrollTop ==== 75){
-		// 	$('#eachDay').append(+populateWeek(newDate, 7)+);
-		// }
-		// $(this).append(week);
-	});
-
 });
 
